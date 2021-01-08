c59fc09fb633e9a0e7d1848965d82d8fa913cbd7
TitoCodes <titosaid2008@gmail.com>
2021-01-07 18:40:58 -0800

    reset function 'space' to reset

--- Slingshot.js
+++ Slingshot.js
@@ -19,6 +19,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    reset(body){
+        this.sling.bodyA = body;
+    }
+
     display(){
         image(this.sling1, 200, 20);
         image(this.sling2, 170, 20);
